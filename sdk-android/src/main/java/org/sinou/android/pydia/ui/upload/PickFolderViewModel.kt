8268386ae42fe08ad45da53dcff35ada3a3fe32f
Bruno Sinou <bruno.sinou@posteo.de>
2022-02-25 11:51:17 +0100

    Better management of force refresh action, factorize backoff ticker

--- sdk-android/src/main/java/org/sinou/android/pydia/ui/upload/PickFolderViewModel.kt
+++ sdk-android/src/main/java/org/sinou/android/pydia/ui/upload/PickFolderViewModel.kt
@@ -1,13 +1,12 @@
 package org.sinou.android.pydia.ui.upload
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.*
 import com.pydio.cells.transport.StateID
 import kotlinx.coroutines.*
 import org.sinou.android.pydia.services.NodeService
+import org.sinou.android.pydia.utils.BackOffTicker
 import java.util.concurrent.TimeUnit
-import kotlin.math.min
 
 /**
  * This holds a folder and all its children folders to choose a target destination
@@ -22,11 +21,13 @@ class PickFolderViewModel(
     val children = nodeService.listChildFolders(stateID)
 
     // Technical local objects
-    private val tag = PickFolderViewModel::class.simpleName
+    // private val tag = PickFolderViewModel::class.simpleName
     private val viewModelJob = Job()
     private val vmScope = CoroutineScope(Dispatchers.Main + viewModelJob)
     private val backOffTicker = BackOffTicker()
     private var _isActive = false
+    private var currWatcher: Job? = null
+
 
     // UI
     private val _isLoading = MutableLiveData<Boolean>()
@@ -45,7 +46,7 @@ class PickFolderViewModel(
             doPull()
             val nd = backOffTicker.getNextDelay()
             delay(TimeUnit.SECONDS.toMillis(nd))
-            Log.e(tag, "... Next delay: $nd")
+            //Log.d(tag, "... Next delay: $nd")
         }
     }
 
@@ -65,7 +66,7 @@ class PickFolderViewModel(
     fun resume() {
         if (!_isActive) {
             _isActive = true
-            watchFolder()
+            currWatcher = watchFolder()
         }
         backOffTicker.resetIndex()
     }
@@ -76,6 +77,8 @@ class PickFolderViewModel(
 
     fun forceRefresh() {
         _isLoading.value = true
+        pause()
+        currWatcher?.cancel()
         resume()
     }
 
@@ -95,31 +98,6 @@ class PickFolderViewModel(
                 return PickFolderViewModel(stateID, nodeService, application) as T
             }
             throw IllegalArgumentException("Unknown ViewModel class")
-        }
-    }
-}
-
-class BackOffTicker {
-    private val backoffDuration = longArrayOf(
-        2, 3, 5, 10, 15, 20,
-        30, 30, 30, 30, 30, 30,
-        40, 60, 60, 120, 120, 300,
-        600, 600, 900, 900, 900, 1800, 3600
-    )
-    private val lock = 0
-    private var currentBackoffIndex = 0
-
-    fun getNextDelay(): Long {
-        synchronized(lock) {
-            val nextDelay = backoffDuration[currentBackoffIndex]
-            currentBackoffIndex = min(currentBackoffIndex + 1, backoffDuration.size - 1)
-            return nextDelay
-        }
-    }
-
-    fun resetIndex() {
-        synchronized(lock) {
-            currentBackoffIndex = 0
         }
     }
 }

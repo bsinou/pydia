7feb04121334a28efa3cc339112208edb86a1f61
Bruno Sinou <bruno.sinou@posteo.de>
2022-03-01 23:33:15 +0100

    Try changing status bar color during search

--- sdk-android/src/main/java/org/sinou/android/pydia/ui/search/SearchFragment.kt
+++ sdk-android/src/main/java/org/sinou/android/pydia/ui/search/SearchFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -77,6 +78,25 @@ class SearchFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         searchVM.query(args.query)
+        val currActivity = requireActivity() as AppCompatActivity
+        val bg = resources.getDrawable(R.drawable.bar_bg_search, requireActivity().theme)
+        currActivity.supportActionBar?.let { bar ->
+            bar.setBackgroundDrawable(bg)
+            bar.title = "Searching: ${args.query}..."
+        }
+        currActivity.window.statusBarColor =  resources.getColor(R.color.searchStatusBarColor)
+        // Requires API level 23
+        // currActivity.window.statusBarColor =  resources.getColor(R.color.danger, requireActivity().theme)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val currActivity = requireActivity() as AppCompatActivity
+        val bg = resources.getDrawable(R.drawable.empty, requireActivity().theme)
+        currActivity.supportActionBar?.let { bar ->
+            bar.setBackgroundDrawable(bg)
+        }
+        currActivity.window.statusBarColor =  resources.getColor(R.color.defaultStatusBarColor)
     }
 
     fun updateQuery(query: String) {

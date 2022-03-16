8b61483552511b706aab9ce4d933542cc8bafe37
Bruno Sinou <bruno.sinou@posteo.de>
2022-03-16 20:13:38 +0100

    Connect session menu to ActiveSession view model

--- sdk-android/src/main/java/org/sinou/android/pydia/ui/menus/ConnectionMenuFragment.kt
+++ sdk-android/src/main/java/org/sinou/android/pydia/ui/menus/ConnectionMenuFragment.kt
@@ -6,9 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import org.sinou.android.pydia.CellsApp
 import org.sinou.android.pydia.R
 import org.sinou.android.pydia.databinding.MoreMenuManageConnectionBinding
+import org.sinou.android.pydia.tasks.loginAccount
+import org.sinou.android.pydia.ui.ActiveSessionViewModel
 
 /**
  * Menu that can be opened when  current session connection is broken to explain status
@@ -17,12 +21,13 @@ import org.sinou.android.pydia.databinding.MoreMenuManageConnectionBinding
 class ConnectionMenuFragment : BottomSheetDialogFragment() {
 
     private val fTag = ConnectionMenuFragment::class.java.simpleName
+    private val activeSessionVM: ActiveSessionViewModel by activityViewModels()
 
     private lateinit var connectionBinding: MoreMenuManageConnectionBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i(fTag, "onCreate ${activeSessionVM.accountId}")
         super.onCreate(savedInstanceState)
-        Log.i(fTag, "onCreate")
     }
 
     override fun onCreateView(
@@ -30,14 +35,23 @@ class ConnectionMenuFragment : BottomSheetDialogFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
+        val accService = CellsApp.instance.accountService
         connectionBinding = DataBindingUtil.inflate(
             inflater, R.layout.more_menu_manage_connection, container, false
         )
-        connectionBinding.launchAuth.setOnClickListener {}
 
+        connectionBinding.session = activeSessionVM.liveSession.value
+        connectionBinding.launchAuth.setOnClickListener {
+            activeSessionVM.liveSession.value?.let {
+                loginAccount(
+                    requireActivity(),
+                    accService.authService,
+                    accService,
+                    it
+                )
+            }
+        }
         connectionBinding.executePendingBindings()
-
         return connectionBinding.root
     }
 
